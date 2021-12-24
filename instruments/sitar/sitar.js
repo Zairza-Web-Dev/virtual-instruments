89153a63993d6b2b60f5926039a5d06b82372ac9
Pallavi Mahapatra <pallu.mahapatra@gmail.com>
2021-12-25 04:41:17 +0530

    message

---
+++ instruments/sitar/sitar.js
@@ -0,0 +1,76 @@
+document.addEventListener("keydown", Key, false);
+
+function Key(e) {
+    if (e.keyCode == "81") {
+        document.getElementById('81').play()
+    }
+    else if (e.keyCode == "87"){
+        document.getElementById('87').play()
+    }
+    else if (e.keyCode == "69"){
+        document.getElementById('69').play()
+    }
+    else if (e.keyCode == "82"){
+        document.getElementById('82').play()
+    }
+    else if (e.keyCode == "84"){
+        document.getElementById('84').play()
+    }
+    else if (e.keyCode == "89"){
+        document.getElementById('89').play()
+    }
+    else if (e.keyCode == "85"){
+        document.getElementById('85').play()
+    }
+    else if (e.keyCode == "73"){
+        document.getElementById('73').play()
+    }
+    else if (e.keyCode == "65"){
+        document.getElementById('65').play()
+    }
+    else if (e.keyCode == "83"){
+        document.getElementById('83').play()
+    }
+    else if (e.keyCode == "68"){
+        document.getElementById('68').play()
+    }
+    else if (e.keyCode == "70"){
+        document.getElementById('70').play()
+    }
+    else if (e.keyCode == "71"){
+        document.getElementById('71').play()
+    }
+    else if (e.keyCode == "72"){
+        document.getElementById('72').play()
+    }
+    else if (e.keyCode == "74"){
+        document.getElementById('74').play()
+    }
+    else if (e.keyCode == "75"){
+        document.getElementById('75').play()
+    }
+    else if (e.keyCode == "90"){
+        document.getElementById('90').play()
+    }
+    else if (e.keyCode == "88"){
+        document.getElementById('88').play()
+    }
+    else if (e.keyCode == "67"){
+        document.getElementById('67').play()
+    }
+    else if (e.keyCode == "86"){
+        document.getElementById('86').play()
+    }
+    else if (e.keyCode == "66"){
+        document.getElementById('66').play()
+    }
+    else if (e.keyCode == "78"){
+        document.getElementById('78').play()
+    }
+    else if (e.keyCode == "77"){
+        document.getElementById('77').play()
+    }
+    else if (e.keyCode == "188"){
+        document.getElementById('188').play()
+    }
+}
